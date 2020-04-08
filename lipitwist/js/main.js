ab8c3ca4b38872d52fcfe1731433b8eddb2983ec
Srishti Sawla <sawlasrishti@gmail.com>
2020-04-08 15:35:42 +0530

    version1.0

--- lipitwist/js/main.js
+++ lipitwist/js/main.js
@@ -26,6 +26,16 @@ options = choosy.map(x=>{
 
 reflection = create(container)('textarea')('reflection')({readonly:true});
 
+footer = create(container)('div')('footer')({});
+
+saver = create(footer)('div')('saver')({});
+saver.innerText = 'Save as Text File';
+
+copier = create(footer)('div')('copier')({});
+copier.innerText = 'Copy to Clipboard';
+
+
+
 twist = (lang) =>(val)=> { 
     if(val==null) reflection.innerHTML = '';
     reflection.innerHTML = devToLang(hkToDev(val))(lang);
@@ -36,10 +46,38 @@ area.onkeyup = () => {
 }
 
 chooser.onchange = () =>{
-    console.log(chooser.value);
     if(chooser.value == 'english'){
         reflection.innerHTML = hkToIast(area.value);
     }
     else reflection.innerHTML = devToLang(hkToDev(area.value))(chooser.value.split(","));
 }
+
+destroyClickedElement = (event) => document.body.removeChild(event.target);
+
+
+saver.onclick = () =>{
+    var textToSave = reflection.value;
+    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    //var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+	var fileNameToSaveAs = "lipiTwist_" + chooser.value[34] + '.txt';
+ 
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+ 
+    downloadLink.click();
+}
+
+copier.onclick = () => {
+    reflection.select();
+    reflection.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+
+}
+
 
